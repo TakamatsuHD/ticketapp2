18b65718f7f699126e7e29357bafd1924ad54347
Hajime Takamatsu <hajimetakamatsu1@gmail.com>
2021-03-18 22:37:25 -0400

    added mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from "express";
 import tickets from "./data/tickets.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
